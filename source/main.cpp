8d27c4f34b43a55c7e6355d0088007007e5d3b22
Fruityloops <fruityloops@littun.co>
2022-02-27 16:06:18 +0100

    coin hook to log the coin amount when new coins are added

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,8 @@
 #include "game/system/GameWork.h"
+#include "piillo/log.h"
 
 int addCoinHook(GameWork* gameWork, int add)
 {
-    gameWork->mCoinCount = 69420;
-    return 69420;
+    piillo::debugLog("Coin Count %d -> %d", gameWork->mCoinCount, gameWork->mCoinCount + add);
+    return gameWork->addCoin(add);
 }
